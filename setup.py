e455f0c4a2abcb2f5eb035468f9e46eb226185a7
whalesarowolo <whalesarowolo@gmail.com>
2023-05-16 23:57:47 +0100

    setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->list[str]:
+    '''return list of requirements'''
+    requirements = []
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+    
+
+setup(
+    name="plantdiseasesdetector",
+    version= '0.0.1',
+    author='Whales',
+    author_email= 'whalesarowolo@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+    )
